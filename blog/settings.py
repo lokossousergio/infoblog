b98dbd0d9b54c506327236350b6b45ce186dd8b8
lokossou <lokossousergio156@gmail.com>
2025-10-28 00:02:45 +0100

    mise a jour

--- blog/settings.py
+++ blog/settings.py
@@ -115,12 +115,13 @@ USE_I18N = True
 
 USE_TZ = False
 
-CSRF_TRUSTED_ORIGINS = ['https://web-production-2c0b.up.railway.app']
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_ROOT=os.path.join(BASE_DIR ,'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
