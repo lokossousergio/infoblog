48f213571ce6d67457a66a134c45d98f69874e87
lokossou <lokossousergio156@gmail.com>
2025-10-28 00:35:42 +0100

    misea jour

--- blog/settings.py
+++ blog/settings.py
@@ -120,8 +120,11 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_ROOT=os.path.join(BASE_DIR ,'static')
+
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
